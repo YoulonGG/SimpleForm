0c026b57dcfb7949420b33b3fd2f44f8b723b274
YoulonGG <johnyoulong@gmail.com>
2024-09-17 14:50:23 +0700

    Initial commit

--- app/src/main/java/com/example/simpleform/FirstActivity.kt
+++ app/src/main/java/com/example/simpleform/FirstActivity.kt
@@ -1,12 +1,15 @@
 package com.example.simpleform
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Calendar
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,25 +22,72 @@ class FirstActivity : AppCompatActivity() {
         val  txtUsername = findViewById<EditText>(R.id.txtUsername)
         val txtAge = findViewById<EditText>(R.id.txtAge)
         val txtJob = findViewById<EditText>(R.id.txtJob)
+        val txtEmail = findViewById<EditText>(R.id.txtEmail)
+        val gender = findViewById<Spinner>(R.id.genderSelection)
+        val datePicker = findViewById<EditText>(R.id.datePicker)
+
+        // Set value to spinner
+        val items = listOf("Male", "Female")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        gender.adapter = adapter
+
+        //Function to date picker
+        fun showDatePickerDialog() {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                datePicker.setText(selectedDate)
+            }, year, month, day)
+
+            datePickerDialog.show()
+        }
+
+        // Function for email need to be valid as email address
+        fun String.isEmailValid(): Boolean {
+            val emailRegex = "^[A-Za-z](.*)(@)(.+)(\\.)(.+)"
+            return emailRegex.toRegex().matches(this)
+        }
+
+        //Show date picker when user clicked
+        datePicker.setOnClickListener {
+            showDatePickerDialog()
+        }
 
         btnSubmit.setOnClickListener {
 
+            //set values to string so we can pass values to other activity
             val userName = txtUsername.text.toString()
             val age = txtAge.text.toString()
             val job = txtJob.text.toString()
+            val email = txtEmail.text.toString()
+            val selectedGender = gender.selectedItem.toString()
+            val dateOfBirth = datePicker.text.toString()
+
+
 
             when {
                 //Check if username input text is empty
                 txtUsername.text.toString().isEmpty() -> txtUsername.error = "Enter your username"
-
                 //let user input not less 4 and not more than 10
                 txtUsername.text.toString().length !in 4..10 -> { txtUsername.error = "Enter between 4 and 10 characters" }
 
+                //Check if email input text is empty
+                txtEmail.text.toString().isEmpty() -> txtEmail.error = "Enter your email"
+                //Check if email is valid
+                !txtEmail.text.toString().isEmailValid() -> txtEmail.error = "Enter your email correctly"
+
                 //check if age input text is empty
                 txtAge.text.toString().isEmpty() -> txtAge.error = "Enter your age"
-
                 //Let user input age only between 18 and 99
                 txtAge.text.toString().toIntOrNull() !in 18..99-> txtAge.error = "Your age must between 18 and 99 years old"
+
+                //Check if date of birth is empty
+                datePicker.text.toString().isEmpty() -> datePicker.error = "Your date of birth is missing!"
 
                 //Check if job input text is empty
                 txtJob.text.toString().isEmpty() -> { txtJob.error = "Enter your job"}
@@ -46,11 +96,15 @@ class FirstActivity : AppCompatActivity() {
 
                     //Give value to Another Activity using putExtra() with key_word
                     intent.putExtra("username_key", userName)
+                    intent.putExtra("email_key", email)
                     intent.putExtra("age_key", age)
+                    intent.putExtra("gender_key", selectedGender)
+                    intent.putExtra("dob_key", dateOfBirth)
                     intent.putExtra("job_key", job)
                     startActivity(intent)
                 }
             }
         }
+
     }
 }
--- app/src/main/java/com/example/simpleform/SecondActivity.kt
+++ app/src/main/java/com/example/simpleform/SecondActivity.kt
@@ -16,14 +16,26 @@ class SecondActivity : AppCompatActivity() {
         val txtUsername = findViewById<TextView>(R.id.txtUsername2)
         val txtAge = findViewById<TextView>(R.id.txtAge2)
         val txtJob = findViewById<TextView>(R.id.txtJob2)
+        val txtEmail = findViewById<TextView>(R.id.txtEmail2)
         val btnBack = findViewById<Button>(R.id.btnBack)
+        val gender = findViewById<TextView>(R.id.txtGender)
+        val dateOfBirth = findViewById<TextView>(R.id.txtDateOfBirth)
 
         //Get value from first Activity using key_word and getStringExtra function
         val getUsername = intent.getStringExtra("username_key")
         txtUsername.text = getUsername
 
+        val getEmail = intent.getStringExtra("email_key")
+        txtEmail.text = getEmail
+
         val getAge = intent.getStringExtra("age_key")
         txtAge.text = getAge
+
+        val getGender = intent.getStringExtra("gender_key")
+        gender.text = getGender
+
+        val getDOB = intent.getStringExtra("dob_key")
+        dateOfBirth.text = getDOB
 
         val getJob = intent.getStringExtra("job_key")
         txtJob.text = getJob
